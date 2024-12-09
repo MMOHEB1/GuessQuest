fd51fe225610c98cbba7072b07547d9a02b54555
mustafa <mohebimustafa12@gmail.com>
2024-12-09 19:32:43 +0000

    commit 3: final product launch

---
+++ gif_bank.py
@@ -0,0 +1,47 @@
+too_low_high = [
+    # pep
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnZsY3VjMnNjcWZxMTg4dnE0cnk1anhndnl1Y2t1c3V5aW9odmg5aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IQYfvE9kIIFCiRweFa/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTJxdWJkdXJqdXV3N243cG56ZWwybmMxazJzZG9uejRmYzZpcWppZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LjJ23de3mvMKNuhSXQ/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExODI4aWZyb3Vlb3RiMnZ5b3M5dWVnaG5oZWFwaWs2cmdpcmsydXBsbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PYmtMMJajjuCjLQOXB/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjFrdnA0MGRvcXlzdmUwaWNoOG0wMXlqZmZlc2FqZzI1NmhyNHkxMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VoFnilMOw9mPC1rW3H/giphy.gif",
+    # mikel
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdm5qYTI3ZWQzcnRnMm41NXdtcHg4ZjY1azl0cnA2czdlcDVkZ2g5cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/A6ymwAjp2pPHEIJHrm/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbzEwYXR0YmpiZjlpN2xyenljeGtsb2psYXFuZWxyeXJ0Y2RpajYyMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/z7Jrl69CD9wqs5C47i/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnBraDB3Z3hqeTN2emlrc2dhaXpydnR2bHM1MDloM20yMWJrem9wbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Fz4ddNtdjhuOPfnjoF/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTNpd2FodTRwM21zdTk3cXZwOWw2andyMHBoZGNka25nYWI0bjQxMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ggEnnB9FHqEdOkLN0s/giphy.gif",
+#     klopp
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2twZjVzMGl2Y3JlOXc5ZDZqaHdqYWRtNXZsZXhzMzQ0ZWNweG0wdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUA7b7eHmShdVRyvQc/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXd6MGY2ZG5zdG9jZnl5bTA4a2ZpMHNibGt5YngxMHZlM3pwOWFnaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/18xXezrUoroeKAXt31/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJqb3QyMXgxYWh0eTN3Z3U4dGE4cWJoZnd1b2Q0emVucW9heDhoZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YRJCQO4S0hlBhPpdB5/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXd6MGY2ZG5zdG9jZnl5bTA4a2ZpMHNibGt5YngxMHZlM3pwOWFnaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/18xXezrUoroeKAXt31/giphy.gif",
+#     ten haag
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnUzNDlldDdudmYyc2lqMm4xeWtwbTh4ejdtYTdyajdrdTg5eWdyMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/v6RkL37LPep5dA4x6y/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTVpejd6aDI3cjcxM3U3NmkzenJ2ZnozeXg2cnN5cGVnc3BnMjF6aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xteHNtzXL6Bq8pU1Vs/giphy-downsized-large.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnBvbTA4MjBrNGl1amdiNWxyeWkzOG5nMHNqaWMwNTFtcGFreDAzZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Gbne6X6RWaUeRdPriJ/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXdiYWN2dDY4bTBkZ3NrMHZvc2dtbmFxOGd1d2JoemlsZnRicjNtYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/AdMhnImyM13HnFWHcH/giphy.gif",
+#     memes
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjlweXd0bGJybzcwbDJtcmpjMTI0cWhmcWFsaDc4cDA3cjBvZjFhNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WxDZ77xhPXf3i/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjcyZ2FtZWV4ZzVoaDU1YWJweHR2MzhiNm83MmwzZTNmYjRkNmh0dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JTTJctRjTatZN4JOGI/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2hhbXdnY2J6Yml2end3M256Y2wzbnlsMGd0OGRjMGxmZ2M4N2kzMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0Vx3zSTdFdnbAlgj3x/giphy.gif",
+    "",
+    "",
+
+]
+
+correct = [
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHRnMzVteXVhaDNlZXkxemZ2bnBqMDh1ejdrOWRwejIydnA3ZGlmcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1AhdEEt6kyr9oY7sT6/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExem93ank2cnZobHc4aXV4OHd5OTlyd2V4M2FlcXNrNGV5bGdtYXJoeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/cQRL8fNySBh8s7Q1U3/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExamtobHM1d2w3NGkyOHkzY3BjdDI3c252aHljYmYzNzc3djhqY2RldCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HSEJPgkpn7LMsIwWQQ/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnl0b2M2M3h1YXRrcTBkOGpseG8xdjduaW96emw2a3h4bG90MGJjcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jzif72fsDMCGmRDXsm/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGV5M3NtZmNkZ3ZxY3JyaDV5eGtoN3VqYWw5azZkamVrNGZwczlneSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3rYvhpvBoURxfM80QA/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3Fld2JmbGtiNHo5NDJtMWlsb3Zidjh0YXZwb2Yyd2oxbDZvbzZhbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0MYIdaxXry7h6eME/giphy.gif",
+]
+
+default_gifs = [
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDMwaWo0cng3OGp0ZWgxMHVvbXUzZWc0ZWt0MHMxa21iM2JmMmhybCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RdYsOsy3S0rte/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExczc3cHhuaThmbXhjbGtmYjFwZDJoeHU3cDkwMmlpcGY0YWd6cnh2bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wXCtara2GzuSR3e82n/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdDRtanQxemRrMnZ4dXRxZGFyZDhvZ2tyazB3eXpxdXNveXZobnAyeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8TY5Uw0CNztcwYuP1t/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGJ3bjZ2cmwyb25qbnZ1Nm5rc3MyeTRwb3U3YmYzaXVrcml2aWpqMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/17r84LXhGZtoWJIlhY/giphy.gif",
+    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXB5N3UzcTV5eXUwNW0xNTQ2d2IyZ2pvcXMwbjI0cnR1ZHEydnVuNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VefU4ZZeZ0kbR5duDs/giphy-downsized-large.gif",
+
+]
--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
 import random
+import gif_bank
 app = Flask(__name__)
+NUMBER = random.randint(0,20)
 
 
-# randnum = random.randint(0, 20)
-# def number_check(func):
-#     def wrapper():
 
 
 @app.route("/")
 def hello_world():
-    return ("""
+    return (f"""
     <div style="
         display: flex;
         flex-direction: column;
@@ -19,24 +18,51 @@ def hello_world():
         height: 100vh;
     ">
         <h1 style="text-align: center;">Guess a number between 0 and 20</h1>
-        <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTA4aGk1amppYzUxa2FwdWg3ZWdnN25idWFpb2NvMnlpc2RxZ3FyOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/nTZt1E0IM8sKSEVirG/giphy.gif" width="500" alt="Guessing Game GIF">
+        <img src={random.choice(gif_bank.default_gifs)} width="500" alt="Guessing Game GIF">
     </div>
     """)
 
 @app.route("/<int:number>")
 def too_low(number):
-    return (f"""
-        <div style="
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        ">
-            <h1 style="text-align: center;">your number: {number}</h1>
-            <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnZsY3VjMnNjcWZxMTg4dnE0cnk1anhndnl1Y2t1c3V5aW9odmg5aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IQYfvE9kIIFCiRweFa/giphy.gif" width="500" alt="Guessing Game GIF">
-        </div>
-        """)
+    if number > NUMBER:
+        return (f"""
+            <div style="
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+            ">
+                <h1 style="text-align: center;">TOO HIGH! TRY AGAIN!</h1>
+                <img src={random.choice(gif_bank.too_low_high)} width="500" alt="Guessing Game GIF">
+            </div>
+            """)
+    elif number < NUMBER:
+        return (f"""
+                    <div style="
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                    ">
+                        <h1 style="text-align: center;">TOO LOW! TRY AGAIN!</h1>
+                        <img src={random.choice(gif_bank.too_low_high)} width="500" alt="Guessing Game GIF">
+                    </div>
+                    """)
+    else:
+        return (f"""
+                    <div style="
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                    ">
+                        <h1 style="text-align: center;">YOU GOT IT!!</h1>
+                        <img src={random.choice(gif_bank.correct)} width="500" alt="Guessing Game GIF">
+                    </div>
+                    """)
 
 if __name__ == "__main__":
     app.run(debug=True)
